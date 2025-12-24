b561aa6e767b9025050482c897034382eb9d18f8
Abhishek688Singh <singh752007abhishek@gmail.com>
2025-12-25 00:23:12 +0530

    fundme frontend

--- index.js
+++ index.js
@@ -10,12 +10,18 @@ withdrawButton.onclick = withdraw
 fundButton.onclick = fund
 balanceButton.onclick = getBalance
 
+const connectedAddress = document.getElementById("connected-address")
+const getFund = document.getElementById("get-fund") 
+const withdrawResult = document.getElementById("withdraw-result") 
+const fundResult = document.getElementById("fund-result") 
+
 async function connect() {
   if (typeof window.ethereum !== "undefined") {
     try {
       await ethereum.request({ method: "eth_requestAccounts" })
       connectButton.innerHTML = "Connected"
       const accounts = await ethereum.request({ method: "eth_accounts" })
+      connectedAddress.innerHTML = accounts
       console.log(accounts)
     } catch (error) {
       console.log(error)
@@ -27,6 +33,7 @@ async function connect() {
 
 async function withdraw() {
   console.log(`Withdrawing...`)
+  withdrawResult.innerHTML = "Withdrawing..."
   if (typeof window.ethereum !== "undefined") {
     const provider = new ethers.BrowserProvider(window.ethereum)
     await provider.send('eth_requestAccounts', [])
@@ -34,11 +41,14 @@ async function withdraw() {
     const contract = new ethers.Contract(contractAddress, abi, signer)
     try {
       console.log("Processing transaction...")
+      withdrawResult.innerHTML = "Processing transaction..."
       const transactionResponse = await contract.withdraw()
       await transactionResponse.wait(1)
       console.log("Done!")
+      withdrawResult.innerHTML = "Withdrawed."
     } catch (error) {
       console.log(error)
+      withdrawResult.innerHTML = "error or You are not owner!!!"
     }
   } else {
     withdrawButton.innerHTML = "Please install MetaMask"
@@ -47,6 +57,7 @@ async function withdraw() {
 
 async function fund() {
   const ethAmount = document.getElementById("ethAmount").value
+  fundResult.innerHTML = `Funding with ${ethAmount}...`
   console.log(`Funding with ${ethAmount}...`)
   if (typeof window.ethereum !== "undefined") {
     const provider = new ethers.BrowserProvider(window.ethereum)
@@ -54,11 +65,15 @@ async function fund() {
     const signer = await provider.getSigner()
     const contract = new ethers.Contract(contractAddress, abi, signer)
     try {
+      fundButton.disabled = true
       const transactionResponse = await contract.fund({
         value: ethers.parseEther(ethAmount),
       })
       await transactionResponse.wait(1)
+      fundResult.innerHTML = "Funded..."
+      fundButton.disabled = false
     } catch (error) {
+      fundResult.innerHTML = "Not enough \"ETH sent < $5 or ~0.003Eth\" or You have Insufficient funds !!!"
       console.log(error)
     }
   } else {
@@ -71,8 +86,10 @@ async function getBalance() {
     const provider = new ethers.BrowserProvider(window.ethereum)
     try {
       const balance = await provider.getBalance(contractAddress)
+      getFund.innerHTML = ethers.formatEther(balance)
       console.log(ethers.formatEther(balance))
     } catch (error) {
+      getFund.innerHTML = "Error fetching Balance"
       console.log(error)
     }
   } else {
